6abab7152c4e2593b222f9f3c1039e61de5ee0da
arthur <arthur@arthurdeMac-mini.local>
2016-11-10 17:18:57 +0800

    init

---
+++ app/src/main/java/com/example/arthur/sampleapp/presenter/LoginPresenter.java
@@ -0,0 +1,7 @@
+package com.example.arthur.sampleapp.presenter;
+
+/**
+ * Created by arthur on 16/11/9.
+ */
+public class LoginPresenter {
+}
---
+++ app/src/main/java/com/example/arthur/sampleapp/view/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.arthur.sampleapp.view;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import com.example.arthur.sampleapp.R;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
+
